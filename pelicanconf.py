c8fe49f51be82a535e4cc56b42e907c1bdcefff7
Abhijit Gadgil <abhijit@hyphenos.io>
2017-08-01 18:14:24 +0530

    Fixed typo in the THEME and reduced the number of links.

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('hyphenOs', 'https://hyphenos.io/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -31,7 +28,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
-THEME = theme/Flex
+THEME = 'themes/Flex'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
