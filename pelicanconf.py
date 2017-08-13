167cd3773b635bccc064d3f6ada4571622e961bb
Abhijit Gadgil <abhijit@hyphenos.io>
2017-08-13 22:55:52 +0530

    started with a very basic hyphenOs theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,7 +28,8 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
-THEME = 'themes/pelican-blue'
+#THEME = 'themes/pelican-blue'
+THEME = 'hyphenOs-theme'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
